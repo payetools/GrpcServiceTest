39705ecbb6e23bf1ca002ca43bc7e493a95a8865
payetools <143715134+payetools@users.noreply.github.com>
2024-12-03 12:56:12 +0100

    fix: updated path for grpc

--- GrpcServiceTest/Program.cs
+++ GrpcServiceTest/Program.cs
@@ -6,15 +6,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGrpc();
 
-
 var app = builder.Build();
 
 app.MapGrpcService<GreeterService>()
+    .RequireHost("*:5128")
     .AllowAnonymous();
 
 app.MapGet("/", async () =>
 {
-    using var channel = GrpcChannel.ForAddress("http://localhost:5068");
+    using var channel = GrpcChannel.ForAddress("https://codefactors-grpc-test.azurewebsites.net:5128");
     var client = new Greeter.GreeterClient(channel);
     var reply = await client.SayHelloAsync(
                       new HelloRequest { Name = "GreeterClient1" });
