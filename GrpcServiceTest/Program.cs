f72a6fae902c46f7eb1166edb10f6e0923690b7b
payetools <143715134+payetools@users.noreply.github.com>
2024-12-04 11:00:57 +0100

    fix: removed port as per Microsoft advice

--- GrpcServiceTest/Program.cs
+++ GrpcServiceTest/Program.cs
@@ -9,12 +9,11 @@ builder.Services.AddGrpc();
 var app = builder.Build();
 
 app.MapGrpcService<GreeterService>()
-    .RequireHost("*:5128")
     .AllowAnonymous();
 
 app.MapGet("/", async () =>
 {
-    using var channel = GrpcChannel.ForAddress("https://codefactors-grpc-test.azurewebsites.net:5128");
+    using var channel = GrpcChannel.ForAddress("https://codefactors-grpc-test.azurewebsites.net");
     var client = new Greeter.GreeterClient(channel);
     var reply = await client.SayHelloAsync(
                       new HelloRequest { Name = "GreeterClient1" });
